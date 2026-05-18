04e62bfaaef10841452d38f307daf55dd182cf34
Kamlesh Prajapati <kamleshprajapati0928@gmail.com>
2026-05-18 14:17:46 +0530

    Extract Firebase private key into variable

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,12 @@
 import admin from "firebase-admin";
 import Logger from "../utils/logger.js";
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  privateKey: privateKey,
 };
 
 try {
